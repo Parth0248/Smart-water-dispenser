8d3f7c2da0f923a363d85279b0ea44a129e2afb9
shavak-kansal <shavakkansal@outlook.com>
2021-12-04 11:48:00 +0530

    .

---
+++ onem2m python files/plot_water_lvl.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import json
+import requests
+from datetime import datetime
+
+ae = "Smart-water-despenser"
+cnt = ["water-level", "when-used", "when-refilled"]
+value_axis = []
+time_axis = []
+
+
+headers = {
+    'X-M2M-Origin' : 'admin:admin',
+    'Content-type' : 'application/json'
+}
+
+response = requests.get(f'http://127.0.0.1:8080/~/in-cse/in-name/{ae}/{cnt[0]}/?rcn=4', headers=headers)
+data = json.loads(response.text)
+
+for i in data['m2m:cnt']['m2m:cin']:
+    time_axis.append(datetime.strptime(i['ct'], '%Y%m%dT%H%M%S'))
+    value_axis.append(int(i['con']))
+    
+
+plt.plot(time_axis, value_axis)
+plt.ylabel('Water level')
+plt.xlabel('Time')
+plt.show()
